5b2641d1262ef8d9b87df38b28df47c36f502566
GEORGE BURTON <georgeburton@live.co.uk>
2021-02-02 23:07:25 +0000

    Added a forecast class

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
     // const cityDetails = data.cityDetails;
@@ -45,21 +46,7 @@ const {cityDetails, weather} = data;
     }
 }
 
-const updateCity = async (city) => {
 
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key)
-
-    return {
-        cityDetails: cityDetails,
-        weather: weather
-
-        // or can use sorthand:
-        // cityDetails,
-        // weather
-    };
-    
-};
 
 cityForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -69,7 +56,7 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // Update the ui with new city
-    updateCity(city)
+    forecast.updateCity(city)
         .then(data => updateUI(data))
         .catch(err => console.log(err))
 
@@ -81,7 +68,7 @@ cityForm.addEventListener('submit', e => {
 });
 
 if(localStorage.getItem('city')){
-    updateCity(localStorage.getItem('city'))
+    forecast.updateCity(localStorage.getItem('city'))
         .then(data => updateUI(data))
         .catch(err => console.log(err));
 }
